66b09dd61c3d97e6c7e5cccf8feec3368fbd6927
AndresJejen <gajcam@gmail.com>
2019-09-10 10:59:10 -0500

    PCD to VTK

---
+++ CMakeFiles/pcd2vtk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcd2vtk.dir/pcd2vtk.cpp.o"
+  "pcd2vtk.pdb"
+  "pcd2vtk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcd2vtk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
